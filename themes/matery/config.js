81643f90a45cb84cbe4cd610741497bd69685810
Cyrusch1 <108743453+Cyrusch1@users.noreply.github.com>
2024-07-15 16:45:48 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -12,18 +12,18 @@ const CONFIG = {
   // 菜单配置
   MATERY_MENU_CATEGORY: true, // 显示分类
   MATERY_MENU_TAG: true, // 显示标签
-  MATERY_MENU_ARCHIVE: true, // 显示归档
-  MATERY_MENU_SEARCH: true, // 显示搜索
+  MATERY_MENU_ARCHIVE: false, // 显示归档
+  MATERY_MENU_SEARCH: false, // 显示搜索
 
   MATERY_POST_LIST_COVER: true, // 文章封面
   MATERY_POST_LIST_SUMMARY: true, // 文章摘要
   MATERY_POST_LIST_PREVIEW: true, // 读取文章预览
 
-  MATERY_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
+  MATERY_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
   MATERY_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  MATERY_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  MATERY_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
-  MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
+  MATERY_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
   MATERY_WIDGET_ANALYTICS: false, // 显示统计卡
   MATERY_WIDGET_TO_TOP: true,
   MATERY_WIDGET_TO_COMMENT: true, // 跳到评论区
